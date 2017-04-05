9c11a1c8c66196d324e913f609c1b8238b2c77a8
Ricky Han <rickylqhan@gmail.com>
2017-04-05 22:09:27 +0000

    wrong file

--- install-cron.sh
+++
@@ -1,2 +0,0 @@
- */10 * * * * /root/blogfetch/.venv/bin/python /root/blogfetch/util/hncrawl.py cron                                                                                                                                                   
- */10 * * * * /root/blogfetch/.venv/bin/python /root/blogfetch/feed.py 
